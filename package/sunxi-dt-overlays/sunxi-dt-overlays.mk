edc6abd817304d348cba534e3a9186a519a98b85
Ulrich Hecht <uli@basicengine.org>
2022-02-02 13:28:46 +0100

    package/sunxi-dt-overlays: install overlays from armbian kernel

--- package/sunxi-dt-overlays/sunxi-dt-overlays.mk
+++ package/sunxi-dt-overlays/sunxi-dt-overlays.mk
@@ -1,16 +1,9 @@
-SUNXI_DT_OVERLAYS_VERSION = master
-SUNXI_DT_OVERLAYS_SITE = $(call github,armbian,sunxi-DT-overlays,$(SUNXI_DT_OVERLAYS_VERSION))
-SUNXI_DT_OVERLAYS_LICENSE = GPL-3.0+
-SUNXI_DT_OVERLAYS_LICENSE_FILES = LICENSE
-SUNXI_DT_OVERLAYS_DEPENDENCIES = host-dtc
+SUNXI_DT_OVERLAYS_VERSION = $(LINUX_VERSION)
+SUNXI_DT_OVERLAYS_DEPENDENCIES = host-dtc linux
 SUNXI_DT_OVERLAYS_INSTALL_IMAGES = YES
 
-define SUNXI_DT_OVERLAYS_BUILD_CMDS
-	for i in $(@D)/*/*.dts ; do $(HOST_DIR)/bin/dtc -O dtb -o "$${i%.dts}.dtbo" -b 0 -@ "$$i" ; done
-endef
-
 define SUNXI_DT_OVERLAYS_INSTALL_IMAGES_CMDS
-	cd $(@D) ; for i in */*.dtbo ; do $(INSTALL) -D -m 0644 "$$i" $(BINARIES_DIR)/overlays/"$$i" ; done
+	cd $(@D)/../linux-$(LINUX_VERSION)/arch/arm/boot/dts/overlay ; for i in *.dtbo ; do x="`echo "$$i"|sed 's,^\([^-]*-[^-]*\)-,\1/,'`" ; $(INSTALL) -D -m 0644 "$$i" $(BINARIES_DIR)/overlays/"$$x" ; done
 endef
 
 $(eval $(generic-package))
