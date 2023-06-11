1ec309b2fd352f5ec0135b62eb1b863170e6c3ba
Ulrich Hecht <uli@basicengine.org>
2023-06-11 11:15:25 +0200

    package/rtl8723as: new package
    This chip is not supported by any other drivers.

---
+++ package/rtl8723as/rtl8723as.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# rtl8723as
+#
+################################################################################
+
+RTL8723AS_VERSION = 4.1.3_6044.20121224
+RTL8723AS_SITE = https://github.com/gabrielepmattia/RTL8723A_WiFi_linux_v$(RTL8723AS_VERSION)/raw/master/driver
+RTL8723AS_SOURCE = rtl8723A_WiFi_linux_v$(RTL8723AS_VERSION).tar.gz
+RTL8723AS_LICENSE = GPL-2.0	# maybe...
+
+RTL8723AS_MODULE_MAKE_OPTS = \
+	KSRC=$(LINUX_DIR) \
+	USER_EXTRA_CFLAGS="-Wno-error"
+
+#	CONFIG_RTL8723AS=m \
+#	KVER=$(LINUX_VERSION_PROBED) \
+
+define RTL8723AS_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NET)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_CFG80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MMC)
+endef
+
+define RTL8723AS_BUILD_CMDS
+	cd $(@D) ; ARCH=arm make CROSS_COMPILE=$(HOST_DIR)/bin/arm-buildroot-linux-gnueabihf- KSRC=$(LINUX_DIR) -j12
+endef
+
+define RTL8723AS_INSTALL_TARGET_CMDS
+	cd $(@D) ; ARCH=arm make CROSS_COMPILE=$(HOST_DIR)/bin/arm-buildroot-linux-gnueabihf- KSRC=$(LINUX_DIR) install MODDESTDIR=$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra
+endef
+
+#$(eval $(kernel-module))
+$(eval $(generic-package))
