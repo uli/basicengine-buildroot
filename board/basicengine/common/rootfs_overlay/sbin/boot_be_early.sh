ab1435cd7df8cb4f6e54ad72ab5292ba4b361b52
Ulrich Hecht <uli@basicengine.org>
2023-09-01 14:12:16 +0200

    board/basicengine: run video recorder at boot

--- board/basicengine/common/rootfs_overlay/sbin/boot_be_early.sh
+++ board/basicengine/common/rootfs_overlay/sbin/boot_be_early.sh
@@ -4,4 +4,5 @@ modprobe jailhouse
 /usr/sbin/jailhouse cell create /etc/jailhouse/orangepi0-inmate-basic.cell 
 /usr/sbin/jailhouse cell load orangepi0-inmate-basic /usr/libexec/jailhouse/demos/beload.bin /lib/firmware/basic.bin -a 0x49000000
 /usr/bin/libc_server &
+/usr/bin/video_recorder &
 /usr/sbin/jailhouse cell start orangepi0-inmate-basic
