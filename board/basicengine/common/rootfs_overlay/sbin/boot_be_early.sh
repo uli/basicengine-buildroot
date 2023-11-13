7980e096a3260ce6e94ac3b4421b74fe2a03cd79
Ulrich Hecht <uli@basicengine.org>
2023-11-13 04:52:38 +0100

    board/basicengine: run init_comms at boot

--- board/basicengine/common/rootfs_overlay/sbin/boot_be_early.sh
+++ board/basicengine/common/rootfs_overlay/sbin/boot_be_early.sh
@@ -3,6 +3,7 @@ modprobe jailhouse
 /usr/sbin/jailhouse enable /etc/jailhouse/orangepi0.cell 
 /usr/sbin/jailhouse cell create /etc/jailhouse/orangepi0-inmate-basic.cell 
 /usr/sbin/jailhouse cell load orangepi0-inmate-basic /usr/libexec/jailhouse/demos/beload.bin /lib/firmware/basic.bin -a 0x49000000
+/usr/bin/init_comms
 /usr/bin/libc_server &
 /usr/bin/video_recorder &
 /usr/sbin/jailhouse cell start orangepi0-inmate-basic
