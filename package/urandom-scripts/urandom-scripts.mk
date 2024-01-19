f53a0c2761b56d53e71a4a402b1b1a1991de60d0
Ulrich Hecht <uli@basicengine.org>
2024-01-19 04:00:48 +0100

    package/urandom-scripts: increase script priority
    udev uses urandom and delays boot unless seeding is done earlier.

--- package/urandom-scripts/urandom-scripts.mk
+++ package/urandom-scripts/urandom-scripts.mk
@@ -6,7 +6,7 @@
 
 define URANDOM_SCRIPTS_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 $(URANDOM_SCRIPTS_PKGDIR)/S20seedrng \
-		$(TARGET_DIR)/etc/init.d/S20seedrng
+		$(TARGET_DIR)/etc/init.d/S05seedrng
 endef
 
 $(eval $(generic-package))
