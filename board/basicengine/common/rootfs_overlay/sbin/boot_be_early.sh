4e91646ec8deb30815e76180bc44c7283dfcf7b8
Ulrich Hecht <uli@basicengine.org>
2024-01-22 10:25:22 +0100

    board/basicengine: boot_be_early.sh: allow for platform-specific init

--- board/basicengine/common/rootfs_overlay/sbin/boot_be_early.sh
+++ board/basicengine/common/rootfs_overlay/sbin/boot_be_early.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+JH_PLATFORM=orangepi0
+test -e /sbin/boot_be_platform.sh && . /sbin/boot_be_platform.sh
+
+# load Jailhouse Engine BASIC cell
 modprobe jailhouse
-/usr/sbin/jailhouse enable /etc/jailhouse/orangepi0.cell 
-/usr/sbin/jailhouse cell create /etc/jailhouse/orangepi0-inmate-basic.cell 
-/usr/sbin/jailhouse cell load orangepi0-inmate-basic /usr/libexec/jailhouse/demos/beload.bin /lib/firmware/basic.bin -a 0x49000000
+/usr/sbin/jailhouse enable /etc/jailhouse/${JH_PLATFORM}.cell
+/usr/sbin/jailhouse cell create /etc/jailhouse/${JH_PLATFORM}-inmate-basic.cell
+/usr/sbin/jailhouse cell load ${JH_PLATFORM}-inmate-basic /usr/libexec/jailhouse/demos/beload.bin /lib/firmware/basic.bin -a 0x49000000
+
+# load essential Linux-side services
 /usr/bin/init_comms
 /usr/bin/libc_server &
 /usr/bin/video_recorder &
-/usr/sbin/jailhouse cell start orangepi0-inmate-basic
+
+# start Engine BASIC
+/usr/sbin/jailhouse cell start ${JH_PLATFORM}-inmate-basic
