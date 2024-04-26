9fb16079e8adc270982f7d79462ab652b6f8885e
Ulrich Hecht <uli@basicengine.org>
2024-04-26 12:37:59 +0200

    board/basicengine/h3_lt: remove various Nano Pis
    These boards have slower DRAM and won't work with the Orange Pi Lite
    parameters.

--- board/basicengine/h3_lt/boot.cmd
+++ board/basicengine/h3_lt/boot.cmd
@@ -23,15 +23,11 @@ else
 	setenv bootmenu_12 Libretech All H3=setenv menu_dtb sun8i-h3-libretech-all-h3-cc.dtb
 	setenv bootmenu_13 Mapleboard MP130=setenv menu_dtb sun8i-h3-mapleboard-mp130.dtb
 	setenv bootmenu_14 NanoPi Duo 2=setenv menu_dtb sun8i-h3-nanopi-duo2.dtb
-	setenv bootmenu_15 NanoPi M1=setenv menu_dtb sun8i-h3-nanopi-m1.dtb
-	setenv bootmenu_16 NanoPi M1 Plus=setenv menu_dtb sun8i-h3-nanopi-m1-plus.dtb
-	setenv bootmenu_17 NanoPi Neo Air=setenv menu_dtb sun8i-h3-nanopi-neo-air.dtb
-	setenv bootmenu_18 NanoPi Neo=setenv menu_dtb sun8i-h3-nanopi-neo.dtb
-	setenv bootmenu_19 NanoPi R1=setenv menu_dtb sun8i-h3-nanopi-r1.dtb
-	setenv bootmenu_20 Revision DVK=setenv menu_dtb sun8i-h3-rervision-dvk.dtb
-	setenv bootmenu_21 ZeroPi=setenv menu_dtb sun8i-h3-zeropi.dtb
-	setenv bootmenu_22 Orange Pi R1=setenv menu_dtb sun8i-h2-plus-orangepi-r1.dtb
-	setenv bootmenu_23 Orange Pi Zero=setenv menu_dtb sun8i-h2-plus-orangepi-zero.dtb
+	setenv bootmenu_15 NanoPi R1=setenv menu_dtb sun8i-h3-nanopi-r1.dtb
+	setenv bootmenu_16 Revision DVK=setenv menu_dtb sun8i-h3-rervision-dvk.dtb
+	setenv bootmenu_17 ZeroPi=setenv menu_dtb sun8i-h3-zeropi.dtb
+	setenv bootmenu_18 Orange Pi R1=setenv menu_dtb sun8i-h2-plus-orangepi-r1.dtb
+	setenv bootmenu_19 Orange Pi Zero=setenv menu_dtb sun8i-h2-plus-orangepi-zero.dtb
 	bootmenu 99
 	saveenv
 	fatload mmc 0 $fdt_addr_r $menu_dtb
